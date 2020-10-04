4481cff0a472ab05a017e490d1c78a87d5e6f2e7
Miha <miha@rothl.com>
2020-10-04 13:06:54 +0100

    feat: add maddpg

--- 01-taxi/main.py
+++ 01-taxi/main.py
@@ -3,6 +3,9 @@ from monitor import interact
 import gym
 import numpy as np
 
-env = gym.make('Taxi-v2')
+env = gym.make('Taxi-v3')
+
+env.render()
+
 agent = Agent()
 avg_rewards, best_avg_reward = interact(env, agent)
